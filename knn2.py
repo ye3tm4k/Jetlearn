37a81f5f26563ce1deadb3f1ffea75b19ec6cd57
Anton <anton.delebarre@isbasel.ch>
2025-12-21 18:47:27 +0100

    21.12.2025

--- knn2.py
+++ knn2.py
@@ -1,7 +1,21 @@
 import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
 
 a = [['apple', 1 , 2], ['Orange', 5 , 4], ['apple', 2 , 1], ['Orange', 6 , 5], ['apple', 1 , 3]]
 x = pd.DataFrame(a, columns=['fruit', 'x','y'])
 target = x['fruit']
 features = x[[ 'x','y']] 
-print(x)
+
+k = KNeighborsClassifier(n_neighbors=3)
+k.fit(features, target)
+y = k.predict([[2,3]])
+
+print(features)
+print(y)
+
+seaborn.scatterplot(x = features['x'], y = features['y'], hue = target)
+plt.scatter(x=2,y=3, color = 'red')
+plt.show()
---
+++ knn3.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+import seaborn
+import matplotlib.pyplot as plt
+
